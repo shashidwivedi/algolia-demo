49098ba05b72620c0014275316bec8f1fddebb7d
Shashi Dwivedi <srdwivedi16@gmail.com>
2024-08-01 22:44:24 +0530

    parallel hits

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const searchClient = algoliasearch(
   'cf8b317ecdb2a2225189ffabd6fd2425'
 );
 
-// main InstantSearch instance
 const search = instantsearch({
   indexName: 'ecommerce',
   searchClient,
@@ -13,37 +12,41 @@ const search = instantsearch({
 });
 
 search.addWidgets([
-  instantsearch.widgets.configure({
-    filters: 'brand:"Incipio"',
-    hitsPerPage: 8,
-  }),
-  // instantsearch.widgets.searchBox({
-  //   container: '#searchbox',
-  //   placeholder: 'Search...',
-  // }),
   instantsearch.widgets.refinementList({
     container: '#categories',
     attribute: 'categories',
     searchable: true,
     searchablePlaceholder: 'Search categories',
   }),
-  instantsearch.widgets.hits({
-    container: '#incipio-hits',
-    templates: {
-      item: (hit, { html, components }) => html`
-        <article>
-          <img src="${hit.image}" alt="${hit.name}" />
-          <div>
-            <h1>${components.Highlight({ hit, attribute: 'name' })}</h1>
-            <p>${components.Highlight({ hit, attribute: 'description' })}</p>
-          </div>
-        </article>
-      `,
-    },
+  instantsearch.widgets.configure({
+    // filters: '(brand:"Incipio" AND brand:"OtterBox")',
+    hitsPerPage: 8,
   }),
-  instantsearch.widgets.pagination({
-    container: '#incipio-pagination',
-  }),
+  instantsearch.widgets
+    .index({ indexName: 'ecommerce' })
+    .addWidgets([
+      instantsearch.widgets.configure({
+        filters: 'brand:"Incipio"',
+        hitsPerPage: 8,
+      }),
+      instantsearch.widgets.hits({
+        container: '#incipio-hits',
+        templates: {
+          item: (hit, { html, components }) => html`
+            <article>
+              <img src="${hit.image}" alt="${hit.name}" />
+              <div>
+                <h1>${components.Highlight({ hit, attribute: 'name' })}</h1>
+                <p>${components.Highlight({ hit, attribute: 'description' })}</p>
+              </div>
+            </article>
+          `,
+        },
+      }),
+      instantsearch.widgets.pagination({
+        container: '#incipio-pagination',
+      }),
+    ]),
   instantsearch.widgets
     .index({ indexName: 'ecommerce' })
     .addWidgets([
